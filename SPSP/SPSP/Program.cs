b262306944fed5e2eb68768152d3add4610d2cf0
unknown <amer.kovac.pmf@gmail.com>
2024-03-27 12:24:54 +0100

    Fix to net 6

--- SPSP/SPSP/Program.cs
+++ SPSP/SPSP/Program.cs
@@ -1,26 +1,109 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using SPSP.Filters;
+using SPSP.Models.SearchObjects;
+using SPSP.Services.Base;
+using SPSP.Services.Reservation.StateMachine;
+using SPSP;
+using SPSP.Services.Customer;
+using SPSP.Services.Database;
+using SPSP.Services.Category;
+using SPSP.Services.Employee;
+using SPSP.Services.Menu;
+using SPSP.Services.Order;
+using SPSP.Services.QRTable;
+using SPSP.Services.Reservation;
+using SPSP.Services.UserAccount;
 
-namespace SPSP
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+builder.Services.AddTransient<IUserAccountService, UserAccountService>();
+builder.Services.AddTransient<ICustomerService, CustomerService>();
+builder.Services.AddTransient<IEmployeeService, EmployeeService>();
+
+
+builder.Services.AddTransient
+    <IService<SPSP.Models.Business, BaseSearchObject>,
+    BaseService<SPSP.Models.Business, Business, BaseSearchObject>>();
+builder.Services.AddTransient<IMenuService, MenuService>();
+builder.Services.AddTransient<IOrderService, OrderService>();
+builder.Services.AddTransient<ICategoryService, CategoryService>();
+builder.Services.AddTransient<IQRTableService, QRTableService>();
+builder.Services.AddTransient<IReservationService, ReservationService>();
+
+builder.Services.AddTransient<BaseState>();
+builder.Services.AddTransient<InitialReservationState>();
+builder.Services.AddTransient<PendingConfirmationReservationState>();
+builder.Services.AddTransient<ConfirmedReservationState>();
+builder.Services.AddTransient<OnHoldReservationState>();
+
+
+builder.Services.AddControllers(x =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    x.Filters.Add<ErrorFilter>();
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("basicAuth", new Microsoft.OpenApi.Models.OpenApiSecurityScheme()
+    {
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+        Scheme = "basic"
+    });
+
+    c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference{Type = ReferenceType.SecurityScheme, Id = "basicAuth"}
+            },
+            new string[]{}
+    } });
+});
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DataDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddAutoMapper(typeof(ICustomerService));
+builder.Services.AddAuthentication("BasicAuthentication")
+    .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    //app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SPSP v1"));
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dataContext = scope.ServiceProvider.GetRequiredService<DataDbContext>();
+    //dataContext.Database.EnsureCreated();
+
+    var conn = dataContext.Database.GetConnectionString();
+
+    dataContext.Database.Migrate();
+
+
+}
+
+app.Run();
